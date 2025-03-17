b621c2bd279ac4bae126849d1e537e6ae62e3cb1
Roy Mor <roym@cs.utexas.edu>
2025-03-16 20:21:07 -0500

    mem-cache: better stats

--- a_scripts/stats.sh
+++ a_scripts/stats.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
 # call from the git repo root
-benches=(429.mcf 450.soplex 471.omnetpp 473.astar 482.sphinx3 483.xalancbmk bfs cc)
+benches=(473.astar bfs cc 429.mcf 471.omnetpp 450.soplex 482.sphinx3 483.xalancbmk)
 cd $1
 
 for benchmark in "${benches[@]}"; do
     echo $benchmark
-    cat $benchmark/stats.txt | grep -E "switch.*ipc" | grep -v commitStats
     cat $benchmark/stats.txt | grep -E "simInsts"
-    cat $benchmark/stats.txt | grep -E "llcache.*overallHits.*total"
+    cat $benchmark/stats.txt | grep -E "numCycles" | grep -v start
+    # cat $benchmark/stats.txt | grep -E "switch.*ipc" | grep -v commitStats
     cat $benchmark/stats.txt | grep -E "llcache.*overallMisses.*total"
-    cat $benchmark/stats.txt | grep -E "llcache.*overallAccesses.*total"
+    cat $benchmark/stats.txt | grep -E "llcache.*overallHits.*total"
+    # cat $benchmark/stats.txt | grep -E "llcache.*overallAccesses.*total"
     echo ""
 done
